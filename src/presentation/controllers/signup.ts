c332e40245e7b66d144d266011fe1fdae8547c6f
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-13 18:47:12 -0300

    refactor: make SignUpController conforms protocol

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
+import { Controller } from '../protocols/controller'
 import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helper/http-helper'
-export class SignUpController {
+
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFileds) {
