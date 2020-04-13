0c3650dd9f8793c17a7065dfe81f5dc5531a9ef4
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-12 21:04:41 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { MissingParamError } from '../errors/missing-params-error'
 import { badRequest } from '../helper/http-helper'
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFileds = ['name', 'email']
+    for (const field of requiredFileds) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
