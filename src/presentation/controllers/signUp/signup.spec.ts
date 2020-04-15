23dbcb4c1a899e0b5272606979290e4b21cf21fc
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-15 08:48:50 -0300

    refactor: move all signUp protocols to a single files

--- src/presentation/controllers/signUp/signup.spec.ts
+++ src/presentation/controllers/signUp/signup.spec.ts
@@ -1,8 +1,11 @@
 import { SignUpController } from './signup'
-import { MissingParamError, InvalidParamError, ServerError } from '../errors'
-import { EmailValidator } from '../protocols'
-import { AddAccount, AddAccountModel } from '../../domain/usecases/add-account'
-import { AccountModel } from '../../domain/models/AccountModel'
+import { MissingParamError, InvalidParamError, ServerError } from '../../errors'
+import {
+  EmailValidator,
+  AddAccount,
+  AddAccountModel,
+  AccountModel
+} from './signup-protocols'
 
 const makeEmailValidatorStub = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
--- src/presentation/controllers/signUp/signup.ts
+++ src/presentation/controllers/signUp/signup.ts
@@ -1,7 +1,12 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols'
-import { MissingParamError, InvalidParamError } from '../errors'
-import { badRequest, serverError } from '../helper/http-helper'
-import { AddAccount } from '../../domain/usecases/add-account'
+import {
+  HttpRequest,
+  HttpResponse,
+  Controller,
+  EmailValidator,
+  AddAccount
+} from './signup-protocols'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, serverError } from '../../helper/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
