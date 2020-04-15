688284e69747b8f99a4504ebe8da6641eee70b31
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-15 19:24:12 -0300

    refactor: move success httpResponse to a helper method

--- src/presentation/controllers/signUp/signup.ts
+++ src/presentation/controllers/signUp/signup.ts
@@ -6,7 +6,7 @@ import {
   AddAccount
 } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError } from '../../helper/http-helper'
+import { badRequest, serverError, ok } from '../../helper/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -34,12 +34,10 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
-      // 36:43
+
       const account = this.addAccount.add({ name, email, password })
-      return {
-        statusCode: 200,
-        body: account
-      }
+
+      return ok(account)
     } catch (error) {
       return serverError()
     }
