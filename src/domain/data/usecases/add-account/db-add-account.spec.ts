2942491b7d6c51b366f3d88c7f3ef8fc174e248b
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-16 08:35:51 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/domain/data/usecases/add-account/db-add-account.spec.ts
+++ src/domain/data/usecases/add-account/db-add-account.spec.ts
@@ -34,4 +34,16 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
+
+  test('should throw if Encrypter throws', async () => {
+    const { sut, encryptStub } = makeStub()
+    jest.spyOn(encryptStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 })
