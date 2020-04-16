7fbe9e27bb21b319065fa372e4d0c352a35cfce5
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-16 20:04:40 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

---
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/usecases/add-account'
+export * from '../../../domain/models/AccountModel'
+export * from '../../protocols/encryptor'
+export * from '../../protocols/add-account-repository'
--- src/domain/data/usecases/add-account/db-add-account-protocols.ts
+++
@@ -1,3 +0,0 @@
-export * from '../../../usecases/add-account'
-export * from '../../../models/AccountModel'
-export * from '../../protocols/encryptor'
