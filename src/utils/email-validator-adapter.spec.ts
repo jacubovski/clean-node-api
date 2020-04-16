a369e83e7a9532de40cb89f86fefa90015303782
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-16 06:51:59 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,7 +3,12 @@ import { EmailValidatorAdapter } from './email-validator'
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValid = sut.isValid('invalid_email@mai')
     expect(isValid).toBe(false)
+  })
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
