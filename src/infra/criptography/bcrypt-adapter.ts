0199c22e1146a9a92938cdabd8ebeddc308e428a
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-17 13:56:26 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,15 @@
+import * as bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encryptor'
+
+export class BcryptAdapter implements Encrypter {
+  private readonly salt: number
+
+  constructor (salt: number) {
+    this.salt = salt
+  }
+
+  async encrypt (value: string): Promise<string> {
+    await bcrypt.hash(value, this.salt)
+    return null
+  }
+}
