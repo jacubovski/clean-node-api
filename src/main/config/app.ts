b775280ecdee9834e88024111c18586b486f14cd
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-04-27 20:50:20 -0300

    feat: ensure requests parse body as json

---
+++ src/main/config/app.ts
@@ -0,0 +1,7 @@
+import * as express from 'express'
+import setupMiddlewares from './middlewares'
+
+const app = express()
+setupMiddlewares(app)
+
+export default app
---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import app from '../config/app'
+import * as request from 'supertest'
+
+describe('Body Parser Middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Breno' })
+      .expect({ name: 'Breno' })
+  })
+})
