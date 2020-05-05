4a6ccd3eaa09bc36fec88360e98f1173f46b6793
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-05-05 07:35:19 -0300

    refactor: add mongodb config

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,8 +3,8 @@ import { MongoClient, Collection } from 'mongodb'
 export const MongoHelper = {
   client: null as MongoClient,
 
-  async connect (uri): Promise<void> {
-    this.client = await MongoClient.connect(uri, {
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
