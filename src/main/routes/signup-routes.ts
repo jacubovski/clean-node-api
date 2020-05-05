63d1139e9dffedc0fe5de30ae37a8453e2f718f7
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-05-05 08:15:50 -0300

    feat: add ExpressRouteAdapter

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
-import { Router, Request, Response } from 'express'
+import { Router } from 'express'
+import { makeSignUpController } from '../factories/signup'
+import { adaptRoute } from '../adapter/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/signup', (req: Request, res: Response) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
