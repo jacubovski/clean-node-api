4556d2f3cce98c9a227e0c0fbaaf3d9e7b0f04c0
Breno Jacubovski <breno.jacubovski@zooxsmart.com>
2020-05-14 19:15:04 -0300

    feat: ensure LogControllerDecorator calls controll er with correct value

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,8 +2,10 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+  url: null as unknown as string,
 
   async connect (url: string): Promise<void> {
+    this.url = url
     this.client = await MongoClient.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true
@@ -14,7 +16,10 @@ export const MongoHelper = {
     await this.client.close()
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.url)
+    }
     return this.client.db().collection(name)
   },
 
